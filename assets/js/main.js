c680ef7337996b8016ee39c506894b7b4c65185a
Orie Chinedu <oriechinedu@gmail.com>
2019-04-30 16:01:22 +0100

    (stretch goal): Modify the scroll speed in the javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,20 +14,20 @@
 		breakpoints({
 			xlarge:  [ '1281px',  '1680px' ],
 			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
+			medium:  [ '500px',   '980px'  ],
+			small:   [ null,      '500px'  ]
 		});
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
-			}, 100);
+			}, 500);
 		});
 
 	// Scrolly.
 		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
+			speed: 1500,
 			offset: function() { return $nav.height(); }
 		});
 
